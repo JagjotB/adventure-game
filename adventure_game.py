07d54cd9f9522141e48f301aa2ba4e89b5892860
JagjotB <jagjot500@gmail.com>
2025-12-25 22:29:24 -0500

    pushing game intro in adventure_game.py

---
+++ adventure_game.py
@@ -0,0 +1,4 @@
+import time
+
+print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
