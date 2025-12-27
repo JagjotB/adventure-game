224bd514eeb264cc7a0b7310d9a0d765699c5046
JagjotB <jagjot500@gmail.com>
2025-12-26 19:28:52 -0500

    added print_pause function

--- adventure_game.py
+++ adventure_game.py
@@ -1,4 +1,8 @@
 import time
 
-print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
+def pause_print(input):
+    time.sleep(1)
+    print(input)
+
+pause_print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+pause_print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
