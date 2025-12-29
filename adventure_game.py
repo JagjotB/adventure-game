1ac4c0f57972be40c701fb1fedb9b0a4ee24216b
JagjotB <jagjot500@gmail.com>
2025-12-29 00:43:59 -0500

    pushing completed code for adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -7,10 +7,13 @@ def pause_print(message):
     time.sleep(1)
     print(message)
 
-pause_print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-pause_print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
-
 def intro():
+    global threat
+    threat = random.choice(creature)
+    
+    pause_print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+    pause_print("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
+
     while True:
         pause_print("Enter 1 to knock on the door of the house.")
         pause_print("Enter 2 to peer into the cave.")
@@ -27,26 +30,40 @@ def intro():
 
         else:
             pause_print("Invalid choice, please try again.")
-            intro()
+            continue
+
+    restart()
 
 def house():
-    threat = random.choice(creature)
     pause_print("You approach the door of the house.")
     pause_print(f"You are about to knock when the door opens and out comes a {threat}!")
-    pause_print(f"Eep! The {threat} has attacked youo!")
+    pause_print(f"Eep! The {threat} has attacked you!")
     pause_print("You have been defeated!")
-    pause_print("Would you like to play again? (y/n)")
-    
 
 def cave():
     pause_print("You peer into the cave.")
     pause_print("It's dark and spooky, but you find a shiny sword!")
     pause_print("You take the sword and feel more confident.")
     pause_print("You approach the door of the house.")
-    pause_print("You are about to knock when the door opens and out steps a wicked witch!")
-    pause_print("You bravely draw your sword to face the wicked witch!")
-    pause_print("After a fierce battle, you have defeated the wicked witch!")
+    pause_print(f"You are about to knock when the door opens and out steps a {threat}!")
+    pause_print(f"You bravely draw your sword to face the {threat}!")
+    pause_print(f"After a fierce battle, you have defeated the {threat}!")
     pause_print("You have saved the village!")
-    pause_print("Would you like to play again? (y/n)")
+
+def restart():
+    while True:
+        pause_print("Would you like to play again? (y/n)")
+        choice = input()
+        if choice == 'y':
+            intro()
+            return
+        
+        elif choice == 'n':
+            pause_print("Thanks for playing! Goodbye.")
+            return
+         
+        else:
+            pause_print("Invalid choice, please try again.")
+            continue
     
 intro()
